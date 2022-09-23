c8cac26809802fdca558a4e9aa53fd40b5a5a7cc
Caio Josef <102381112+caiojosef@users.noreply.github.com>
2022-09-23 19:44:01 -0300

    códigos finalizados

--- Projetos Java Faculdade/aula 09-09/src/aulaNoveDoNove.java
+++ Projetos Java Faculdade/aula 09-09/src/aulaNoveDoNove.java
@@ -6,40 +6,56 @@ public class aulaNoveDoNove {
 			System.out.print(" " + i);
 		}
 		
-		System.out.println("\nContador de 50 até 1: ");
+		System.out.println("\n\nContador de 50 até 1: ");
 		for(int i = 50; i >= 1; i--) {
 			System.out.print(" " + i);
 		}
 		
-		System.out.println("\nContador de 3 em 3: ");
+		System.out.println("\n\nContador de 3 em 3: ");
 		for(int i = 0; i <= 50; i++) {
 			System.out.print(" " + i++);
 			i++;
 		}
 		int j = 0;
-		System.out.println("\nIncrementando +1 +2 +3 e assim sucessivamente ");
-		for(int i = 1; i <= 1000; i++) {
+		System.out.println("\n\nIncrementando +1 +2 +3 e assim sucessivamente ");
+		for(int i = 1; j < 1000; i++) {
 			System.out.print(" " + j);
-			j+=i;
-			if(j >= 1000)
-				break;
+			j += i;
+			
 		}	
 		
-		int linha = 4;
-		int coluna = 4;
-		System.out.println("\nQuadrado");
+		int linha = 6;
+
+		System.out.println("\n\nQuadrado");
 			
 		
-		for(int i = 0; i <= linha; i++) {
-			System.out.print("*");
-			for(int k = 0; k <= coluna; k++) {
-				System.out.print("*");
+		for(int i = 0; i < linha; i++) {
+			for(int k = 0; k < linha; k++) {
+				System.out.print("* ");
 			}
-			
+			System.out.println(" ");
 		}
-			
-			
-				
 		
+		System.out.println("\n\nQuadrado vazado");
+		
+		int linhas = 10;
+		for(int i = 0; i < linhas; i++)
+			for(int k = 0; k < linhas; k++) {
+				if(i == 0 || i == (linhas - 1)) {
+					System.out.print("* ");
+				} else {
+					if(k == 0 || k == (linhas - 1)) {
+						System.out.print("* ");
+					}else {
+						if(i == k) {
+							System.out.print("* ");
+						} else {
+							System.out.print(" ");
+						}
+					}
+					
+				}
+					
+			}	
 	}
 }
